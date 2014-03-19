63a3d684d0652d7771105b1c4aeb051d5350b14c
Sankalp Kulshreshtha <sankalp221@gmail.com>
2014-03-19 12:03:51 -0400

    Devise authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Gelfand::Application.routes.draw do
+  devise_for :users
+  root :to => "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +56,6 @@ Gelfand::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
 end

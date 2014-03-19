674b38bd356046ce198ac12065b4d973c116fc8e
Sankalp Kulshreshtha <sankalp221@gmail.com>
2014-03-19 11:45:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gelfand::Application.config.secret_key_base = 'b2852bcbf7b4b824a695ea1da7988f890b39e902faa973c166ef9b7f9c4acf42246e92403b1fcc724093de3ba52022a907458d4d42c7cdcccf8bdbdf4925de22'

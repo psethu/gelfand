1c472ca6779fa067ac100a2fd5c0bad721cea94c
cjwillia <cjwillia@andrew.cmu.edu>
2014-03-24 17:08:01 -0400

    added throughs to many-to-many relationships

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,6 +3,8 @@ class Program < ActiveRecord::Base
     # Relationships
     # ------------- 
     has_many :participants
+    has_many :individual, through: :participants
     has_many :affiliations
+    has_many :organizations, through: :affiliations
     
 end

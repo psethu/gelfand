1f1084431498e3e2d7c7c3061f2d5caf116525ec
cjwillia <cjwillia@andrew.cmu.edu>
2014-03-24 17:05:04 -0400

    added throughs to many-to-many relationships

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -4,6 +4,8 @@ class Individual < ActiveRecord::Base
     # -------------
     belongs_to :contact
     has_many :memberships
+    has_many :organizations, through: :memberships
     has_many :participants
+    has_many :projects, through: :participants
     has_one :bg_check
 end

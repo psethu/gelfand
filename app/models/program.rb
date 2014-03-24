32a5f2a4e5b98edc8ac51d78c1a28205e317302d
jschiang <jerm6002@gmail.com>
2014-03-24 17:10:49 -0400

    Fixed a typo for individuals on Program

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,7 +3,7 @@ class Program < ActiveRecord::Base
     # Relationships
     # ------------- 
     has_many :participants
-    has_many :individual, through: :participants
+    has_many :individuals, through: :participants
     has_many :affiliations
     has_many :organizations, through: :affiliations
     

4fdfa8768834a8ee2ac518ea43ece55c7be0f8fe
jschiang <jschiang1@gmail.com>
2014-03-27 19:43:21 -0400

    started individuals model, made some changes to view

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -9,4 +9,18 @@ class Individual < ActiveRecord::Base
     has_many :programs, through: :participants
     has_one :bg_check
 
+    validates :f_name, :presence => true
+    validates :l_name, :presence => true
+    validates :role, :presence => true, :numericality => {:only_integer => true, :less_than_or_equal_to => 2, :greater_than_or_equal_to => 0}
+    validates_date :dob, :on_or_before => lambda {Date.current}
+
+
+    scope :alphabetical, order('l_name, f_name')
+    scope :active, where('active = ?', true)
+    scope :no_bg_check, where('bg_check_id IS NULL')
+
+    def name
+    	"#{l_name}, #{f_name}"
+    end
+
 end

35b63c2077ccb3c90e38730381ac9aef0986819c
jschiang <jschiang1@gmail.com>
2014-04-28 17:05:03 -0400

    bg_checks link to proper forms, and admin redirects to bg_check index not show

--- app/controllers/bg_checks_controller.rb
+++ app/controllers/bg_checks_controller.rb
@@ -20,7 +20,7 @@ class BgChecksController < ApplicationController
     respond_to do |format|
       if @bg_check.save
 
-        format.html { redirect_to @bg_check, notice: 'Bg check was successfully created.' }
+        format.html { redirect_to @bg_checks, notice: 'Bg check was successfully created.' }
         format.json { render action: 'show', status: :created, location: @bg_check }
       else
         format.html { render action: 'new' }
@@ -34,7 +34,7 @@ class BgChecksController < ApplicationController
   def update
     respond_to do |format|
       if @bg_check.update(bg_check_params)
-        format.html { redirect_to @bg_check, notice: 'Bg check was successfully updated.' }
+        format.html { redirect_to bg_checks_path, notice: 'Bg check was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }

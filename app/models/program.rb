302b9d2885b6ce02d7c37e637775ff4c16b32eb9
Sethu Prakasam <psethu27@gmail.com>
2014-05-04 23:07:26 -0400

    Fixed url for ongoing,completed,upcoming programs

--- app/models/program.rb
+++ app/models/program.rb
@@ -14,6 +14,7 @@ class Program < ActiveRecord::Base
     # -----------
     
     validates :name, :presence => true
+    validates_uniqueness_of :name
     validates :num_minors, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
 
     validates_date :start_date, :presence => true
